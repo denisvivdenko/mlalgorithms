acf12d7c2c7ae8bd0516a5a5f7b05dcb5ce82fcd
Denis Vivdenko <denis.vivdenko.freelancer@gmail.com>
2021-06-15 14:12:34 +0300

    add spam message parser that parses data to csv

---
+++ NaiveBayesClassifier/Message.py
@@ -0,0 +1,5 @@
+class Message:
+
+    def __init__(self, file_path, is_spam):
+        self.file_path = file_path
+        self.is_spam = is_spam
---
+++ NaiveBayesClassifier/NaiveBayesClassifier.py
@@ -0,0 +1,13 @@
+class NaiveBayesClassifier:
+
+    def __init__(self):
+        pass
+
+    def fit(self, X, y):
+        pass
+
+    def predict_list(self, X):
+        pass
+
+    def predict(self, X):
+        pass
